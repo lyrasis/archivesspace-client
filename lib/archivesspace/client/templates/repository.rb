57e6aa30d552d2153968f3739036bc5a687c3a4b
Mark Cooper <mark.c.cooper@outlook.com>
2014-03-30 14:22:11 -0700

    Add initial repository template

---
+++ lib/archivesspace/client/templates/repository.rb
@@ -0,0 +1,30 @@
+{
+  repository: {
+    repo_code: "",
+    name: "",
+    org_code: "",
+    country: "",
+    parent_institution_name: "",
+    url: "",
+  },
+  agent_representation: {
+    names: [ { primary_name: "", source: "local", sort_name: "" } ],
+    agent_contacts: [
+      {
+        name: "",
+        address_1: "",
+        address_2: "",
+        address_3: "",
+        city: "",
+        region: "",
+        country: "",
+        post_code: "",
+        telephone: "",
+        telephone_ext: "",
+        fax: "",
+        email: "",
+        note: "",
+      }
+    ]
+  }
+}
