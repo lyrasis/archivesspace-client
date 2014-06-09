50fb761cc54624ce172bd54a7cdaec8353f0d7fd
Mark Cooper <markchristophercooper@gmail.com>
2014-06-08 17:39:23 -0700

    Bring examples more up to date

--- examples/perms_and_groups.rb
+++ examples/perms_and_groups.rb
@@ -3,57 +3,10 @@ require "archivesspace/client"
 require "json"
 require "pp"
 
-host             = "localhost"
-backend_port     = "8089" 
-backend          = ArchivesSpace::Client.new( { site: "http://#{host}:#{backend_port}" } )
-backend.user     = 'admin'
-backend.password = 'admin'
-backend.login
+client          = ArchivesSpace::Client.new # localhost,  8089
+client.login "admin", "admin"
 
-# will fail silently after 1st run but safe as retrieved below
-repo = backend.create(:repository, { repository: { repo_code: 'EXAMPLE', name: 'Example Archive' } })
-
-_p = {}
-_g = {}
-
-ver    = backend.version
-repo   = backend.repositories.find { |r| r.name =~ /example/i }
-perms  = backend.permissions "all"
-groups = backend.groups(repo)
-
-perms.each do |p|
-  _p[p.permission_code] = {} unless _p.has_key? p.permission_code
-  _p[p.permission_code][:description] = p.description.split(":")[0]
-  _p[p.permission_code][:level]       = p.level
-  _p[p.permission_code][:uri]         = p.uri
-end
-
-groups.each do |g|
-  _g[g.group_code] = {} unless _g.has_key? g.group_code
-  _g[g.group_code][:description] = g.description.split(":")[0]
-  _g[g.group_code][:permissions] = g.grants_permissions
-end
-
-puts
-
-puts "#{ver} permissions and groups"
-
-puts
-
-_p.each do |code, attributes|
-  puts "#{code}\t#{attributes[:description]}\t#{attributes[:level]}"
-end
-
-puts
-
-_g.each do |code, attributes|
-  puts code
-  puts "\t#{attributes[:description]}"
-  attributes[:permissions].each do |p|
-    puts "\t\t#{_p[p][:description]}"
-  end
-  puts
-end
+##### COMING ...
 
 __END__
 
--- examples/repo_and_user.rb
+++ examples/repo_and_user.rb
@@ -2,94 +2,33 @@
 require "archivesspace/client"
 require "pp"
 
-host             = "localhost"
-backend_port     = "8089" 
-backend          = ArchivesSpace::Client.new( { site: "http://#{host}:#{backend_port}" } )
-backend.user     = 'admin'
-backend.password = 'admin'
-backend.login
+client          = ArchivesSpace::Client.new # localhost,  8089
+client.login "admin", "admin"
 
 ########## EXAMPLES USING PROVIDED TEMPLATES
 
-pp backend.templates # view available templates
+pp client.templates # view available templates
 
-x = backend.template_for "repository"
-x.repository["repo_code"] = "XYZ"
-x.repository["name"] = "XYZ Archive"
-x.agent_representation["names"][0]["primary_name"] = "XYZ"
-x.agent_representation["names"][0]["sort_name"] = "XYZ"
-x.agent_representation["agent_contacts"][0]["name"] = "Lionel Messi"
+repository_with_agent = client.template_for "repository_with_agent"
+repository_with_agent["repository"]["repo_code"] = "XYZ"
+repository_with_agent["repository"]["name"] = "XYZ Archive"
+repository_with_agent["agent_representation"]["names"][0]["primary_name"] = "XYZ"
+repository_with_agent["agent_representation"]["names"][0]["sort_name"] = "XYZ"
+repository_with_agent["agent_representation"]["agent_contacts"][0]["name"] = "Lionel Messi"
 
-pp backend.create_from_template x
-pp backend.repositories.find { |r| r.name =~ /xyz/i } 
+pp client.create "repository_with_agent", repository_with_agent
+pp client.repositories_with_agent.find { |r| r["name"] =~ /xyz/i } 
 
-u = backend.template_for "user"
-u.username = "lmessi"
-u.name = "Lionel Messi"
-u.is_admin = true
-pp backend.create_from_template u, { password: "123456" }
-pp backend.users.find { |u| u.username =~ /messi/i } # see below for update example 
+user = client.template_for "user"
+user["username"] = "lmessi"
+user["name"] = "Lionel Messi"
+user["is_admin"] = true
+pp client.create "user", u, { password: "123456" }
 
-########## EXAMPLES WITH USER SUPPLIED DATA STRUCTURE
+user = client.users.find { |u| u.username =~ /messi/i }
+user["name"] = "Cristiano Ronaldo"
 
-# repository: repo_code and name are required
-# agent representation is not required at all
-repo = {
-  repository: {
-    repo_code: "ABC",
-    name: "ABC Archive",
-    org_code: "",
-    country: "US",
-    parent_institution_name: "",
-    url: "http://www.abc.org",
-  },
-  agent_representation: {
-    names: [ { primary_name: "ABC Archive", source: "local", sort_name: "ABC Archive" } ],
-    agent_contacts: [
-      {
-        name: "A User",
-        address_1: "Some Deptartment",
-        address_2: "Some street",
-        address_3: "",
-        city: "Some City",
-        region: "Some State",
-        country: "United States",
-        post_code: "12345",
-        telephone: "777-777-7777",
-        telephone_ext: "",
-        fax: "",
-        email: "auser@abc.org",
-        note: "",
-      }
-    ]
-  }
-}
-
-pp backend.create(:repository, repo)
-pp backend.repositories.find { |r| r.name =~ /abc/i }
-
-# username and name are required
-# password must be submitted via params
-user = {
-  username: "auser",
-  name: "A User",
-  email: "auser@abc.org",
-  first_name: "A",
-  last_name: "User",
-  telephone: "777-777-7777",
-  title: "Manager",
-  department: "ABC Archive",
-  additional_contact: "A good egg",
-  is_admin: false,
-}
-pw = "654321"
-
-pp backend.create(:user, user, { password: pw })
-auser = backend.users.find { |u| u.username =~ /auser/i }
-
-auser.title = "ABC Archive Manager"
-pw = "123456"
-pp backend.update(auser, { password: pw })
+pp client.update user
 
 __END__
 
