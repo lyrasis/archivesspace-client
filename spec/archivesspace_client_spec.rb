d41e05bff38ccd90ebc69756c7ee71368834e940
Mark Cooper <markchristophercooper@gmail.com>
2014-07-30 09:01:00 -0700

    Add more pending tests

--- spec/archivesspace_client_spec.rb
+++ spec/archivesspace_client_spec.rb
@@ -14,6 +14,10 @@ def create_user!
   @client.create "user", user, { password: "123456" }  
 end
 
+def create_digital_object!(repository, title)
+  digital_object = @client.template_for "digital_object"
+end
+
 def delete!(object)
   @client.delete object
 end
@@ -25,36 +29,89 @@ describe "interacting with the api" do
     @client.login "admin", "admin"
   end
 
-  it "requesting version should retrieve version information" do
-    expect(@client.version["databaseProductName"]).to eq "Apache Derby"
+  context "System" do
+
+    it "requesting version should retrieve version information" do
+      expect(@client.version["databaseProductName"]).to eq "Apache Derby"
+    end
+
   end
 
-  it "should allow a repository to be created from a template, updated and deleted" do
-    repository = create_repository!
-    expect(repository["repo_code"]).to eq "ABC"
+  context "Repositories" do
 
-    repository["name"] = "DEF Archives"
-    repository = @client.update repository
-    expect(repository["name"]).to eq "DEF Archives"
+    it "should allow a repository to be created from a template, updated and deleted" do
+      repository = create_repository!
+      expect(repository["repo_code"]).to eq "ABC"
 
-    result = delete! repository
-    expect(result["status"]).to eq "Deleted"
+      repository["name"] = "DEF Archives"
+      repository = @client.update repository
+      expect(repository["name"]).to eq "DEF Archives"
+
+      result = delete! repository
+      expect(result["status"]).to eq "Deleted"
+    end
+
   end
 
-  it "should allow a user to be created from a template, updated and deleted" do
-    user = create_user!
-    expect(user["username"]).to eq "lmessi"
-    
-    user["name"] = "Cristiano Ronaldo"
-    user = @client.update user
-    expect(user["name"]).to eq "Cristiano Ronaldo"
+  context "Users" do
 
-    result = delete! user
-    expect(result["status"]).to eq "Deleted"
+    it "should allow a user to be created from a template, updated and deleted" do
+      user = create_user!
+      expect(user["username"]).to eq "lmessi"
+
+      user["name"] = "Cristiano Ronaldo"
+      user = @client.update user
+      expect(user["name"]).to eq "Cristiano Ronaldo"
+
+      result = delete! user
+      expect(result["status"]).to eq "Deleted"
+    end
+
   end
 
-  it "should retrieve permissions", pending: true do
-    @client.permissions
+  context "Working with Repository objects" do
+
+    before :all do
+      @repository = create_repository!
+    end
+
+    after :all do
+      delete! @repository
+    end
+
+    context "Digital Objects" do
+
+      it "should allow a digital object to be created from a template, updated and deleted", pending: true do
+        digital_object = create_digital_object!(@repository, "The Moon")
+        expect(digital_object["title"]).to eq "The Moon"
+
+        digital_object["name"] = "The Earth"
+        digital_object = @client.update digital_object
+        expect(digital_object["title"]).to eq "The Earth"
+
+        result = delete! digital_object
+        expect(result["status"]).to eq "Deleted"
+      end
+
+      it "should retrieve digital objects", pending: true do
+        digital_object1 = create_digital_object!(@repository, "The Moon")
+        digital_object2 = create_digital_object!(@repository, "The Earth")
+        digital_objects = @client.digital_objects(@repository)
+        expect(digital_objects.size).to eq 2
+        delete! digital_object1
+        delete! digital_object2
+      end
+
+    end
+
+  end
+
+  context "Permissions" do
+
+    it "should retrieve permissions", pending: true do
+      @client.permissions
+    end
+
   end
 
 end
