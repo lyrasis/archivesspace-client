d9e4a69f35fd6331dd483c93bf530e58705e2331
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2026-02-11 16:57:54 -0800

    Cleanup config init

--- lib/archivesspace/client/configuration.rb
+++ lib/archivesspace/client/configuration.rb
@@ -2,27 +2,26 @@
 
 module ArchivesSpace
   class Configuration
-    def defaults
-      {
-        base_uri: "http://localhost:8089",
-        base_repo: "",
-        debug: false,
-        username: "admin",
-        password: "admin",
-        page_size: 50,
-        throttle: 0,
-        verify_ssl: true,
-        timeout: 60
-      }
-    end
+    attr_accessor :base_uri, :base_repo, :debug, :username, :password,
+      :page_size, :throttle, :verify_ssl, :timeout
+
+    DEFAULTS = {
+      base_uri: "http://localhost:8089",
+      base_repo: "",
+      debug: false,
+      username: "admin",
+      password: "admin",
+      page_size: 50,
+      throttle: 0,
+      verify_ssl: true,
+      timeout: 60
+    }.freeze
 
     def initialize(settings = {})
-      settings = defaults.merge(settings)
-      settings.each do |property, value|
-        next unless defaults.key?(property)
+      DEFAULTS.merge(settings).each do |property, value|
+        next unless DEFAULTS.key?(property)
 
-        instance_variable_set(:"@#{property}", value)
-        self.class.send(:attr_accessor, property)
+        send(:"#{property}=", value)
       end
     end
   end
